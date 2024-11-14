c6b9fb1fd27bcbd4777b9278dee4567012993f03
LeroyBG <leroylightning@g.ucla.edu>
2024-11-13 18:56:41 -0800

    trailpower base.oml syntaxi highlighting working

--- oml-code/src/cli/main.ts
+++ oml-code/src/cli/main.ts
@@ -1,11 +1,11 @@
-import type { Model } from '../language/generated/ast.js';
-import chalk from 'chalk';
+// import type { Ontology } from '../language/generated/ast.js';
+// import chalk from 'chalk';
 import { Command } from 'commander';
 import { OntologicalModelingLanguageLanguageMetaData } from '../language/generated/module.js';
-import { createOntologicalModelingLanguageServices } from '../language/ontological-modeling-language-module.js';
-import { extractAstNode } from './cli-util.js';
-import { generateJavaScript } from './generator.js';
-import { NodeFileSystem } from 'langium/node';
+// import { createOntologicalModelingLanguageServices } from '../language/ontological-modeling-language-module.js';
+// import { extractAstNode } from './cli-util.js';
+// import { generateJavaScript } from './generator.js';
+// import { NodeFileSystem } from 'langium/node';
 import * as url from 'node:url';
 import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
@@ -14,12 +14,12 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const packagePath = path.resolve(__dirname, '..', '..', 'package.json');
 const packageContent = await fs.readFile(packagePath, 'utf-8');
 
-export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
-    const services = createOntologicalModelingLanguageServices(NodeFileSystem).OntologicalModelingLanguage;
-    const model = await extractAstNode<Model>(fileName, services);
-    const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
-    console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
-};
+// export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
+//     const services = createOntologicalModelingLanguageServices(NodeFileSystem).OntologicalModelingLanguage;
+//     const model = await extractAstNode<Model>(fileName, services);
+//     const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
+//     console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
+// };
 
 export type GenerateOptions = {
     destination?: string;
@@ -32,11 +32,10 @@ export default function(): void {
 
     const fileExtensions = OntologicalModelingLanguageLanguageMetaData.fileExtensions.join(', ');
     program
-        .command('generate')
+        .command('parseAndValidate')
         .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
-        .option('-d, --destination <dir>', 'destination directory of generating')
-        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
-        .action(generateAction);
+        .description('Indicates where a program parses & validates successfully, but produces no output code')
+        // .action(generateAction);
 
     program.parse(process.argv);
 }
--- oml-code/src/language/ontological-modeling-language-validator.ts
+++ oml-code/src/language/ontological-modeling-language-validator.ts
@@ -1,17 +1,17 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { OntologicalModelingLanguageAstType, Person } from './generated/ast.js';
+// import type { ValidationAcceptor, ValidationChecks } from 'langium';
+// import type { OntologicalModelingLanguageAstType } from './generated/ast.js';
 import type { OntologicalModelingLanguageServices } from './ontological-modeling-language-module.js';
 
 /**
  * Register custom validation checks.
  */
 export function registerValidationChecks(services: OntologicalModelingLanguageServices) {
-    const registry = services.validation.ValidationRegistry;
-    const validator = services.validation.OntologicalModelingLanguageValidator;
-    const checks: ValidationChecks<OntologicalModelingLanguageAstType> = {
-        // Person: validator.checkPersonStartsWithCapital
-    };
-    registry.register(checks, validator);
+    // const registry = services.validation.ValidationRegistry;
+    // const validator = services.validation.OntologicalModelingLanguageValidator;
+    // const checks: ValidationChecks<OntologicalModelingLanguageAstType> = {
+    //     // Person: validator.checkPersonStartsWithCapital
+    // };
+    // registry.register(checks, validator);
 }
 
 /**
@@ -19,13 +19,8 @@ export function registerValidationChecks(services: OntologicalModelingLanguageSe
  */
 export class OntologicalModelingLanguageValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+    // checkPersonStartsWithCapital(accept: ValidationAcceptor): void {
+        
+    // }
 
 }
