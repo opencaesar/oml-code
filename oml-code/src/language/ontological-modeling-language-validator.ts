94efc0c7e41c6c6b0eb9ea17945b3d425f623883
LeroyBG <leroylightning@g.ucla.edu>
2025-01-24 16:21:43 -0800

    validation rule ensures ScalarEquivalenceAxiom properties occur at most once

--- oml-code/src/language/ontological-modeling-language-validator.ts
+++ oml-code/src/language/ontological-modeling-language-validator.ts
@@ -1,17 +1,19 @@
 // import type { ValidationAcceptor, ValidationChecks } from 'langium';
 // import type { OntologicalModelingLanguageAstType } from './generated/ast.js';
+import { ValidationAcceptor, ValidationChecks } from 'langium';
 import type { OntologicalModelingLanguageServices } from './ontological-modeling-language-module.js';
+import { OntologicalModelingLanguageAstType, ScalarEquivalenceAxiom } from './generated/ast.js';
 
 /**
  * Register custom validation checks.
  */
 export function registerValidationChecks(services: OntologicalModelingLanguageServices) {
-    // const registry = services.validation.ValidationRegistry;
-    // const validator = services.validation.OntologicalModelingLanguageValidator;
-    // const checks: ValidationChecks<OntologicalModelingLanguageAstType> = {
-    //     // Person: validator.checkPersonStartsWithCapital
-    // };
-    // registry.register(checks, validator);
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.OntologicalModelingLanguageValidator;
+    const checks: ValidationChecks<OntologicalModelingLanguageAstType> = {
+        ScalarEquivalenceAxiom: validator.checkUniqueScalarEquivalenceProperties
+    };
+    registry.register(checks, validator);
 }
 
 /**
@@ -19,8 +21,32 @@ export function registerValidationChecks(services: OntologicalModelingLanguageSe
  */
 export class OntologicalModelingLanguageValidator {
 
-    // checkPersonStartsWithCapital(accept: ValidationAcceptor): void {
+    // A ScalarEquivalenceAxiom can only have one instance of each of the
+    // properties listed in uniqueProps. This validation rule addresses
+    // the fact that optional elements in unordered groups aren't supported
+    // by langium
+    checkUniqueScalarEquivalenceProperties(axiom: ScalarEquivalenceAxiom, accept: ValidationAcceptor): void {
+        // Note: this list will need to be updated if more properties are added to the ScalarEquivalenceAxiom rule
+        const uniqueProps = [
+            'length',
+            'minLength',
+            'maxLength',
+            'pattern',
+            'language',
+            'minInclusive',
+            'minExclusive',
+            'maxInclusive',
+            'maxExclusive'
+        ]
         
-    // }
+        for (const prop of uniqueProps) {
+            // @ts-ignore
+            if (axiom[prop].length > 1) {
+                accept('error', `specifiy at most one of each property (duplicate ${prop})`, {
+                    node: axiom
+                })
+            }
+        }
+    }
 
 }
