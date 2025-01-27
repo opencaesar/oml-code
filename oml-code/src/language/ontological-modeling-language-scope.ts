19fcab0269da536511e62de1e6c4c890ced7730f
LeroyBG <leroylightning@g.ucla.edu>
2025-01-26 17:11:56 -0800

    isolate scoping bug

--- oml-code/src/language/ontological-modeling-language-scope.ts
+++ oml-code/src/language/ontological-modeling-language-scope.ts
@@ -1,82 +1,13 @@
-import { AstNode, AstNodeDescription, AstUtils, DefaultScopeComputation, DefaultScopeProvider, LangiumDocument, ReferenceInfo, Scope } from "langium";
-import { isImport, isOntology, OntologicalModelingLanguageTerminals, Ontology } from "./generated/ast.js";
+import { AstNodeDescription, DefaultScopeComputation, LangiumDocument } from "langium";
+import { LangiumServices } from "langium/lsp";
+import { CancellationToken } from "vscode-languageclient";
 
 export class OMLScopeComputation extends DefaultScopeComputation {
-    protected override exportNode(node: AstNode, exports: AstNodeDescription[], document: LangiumDocument): void {
-        if (isOntology(node) && node.namespace) {
-            const { namespace } = node
-            exports.push(this.descriptions.createDescription(node, namespace, document))
-        } else {
-            const unqualifiedName: string | undefined = this.nameProvider.getName(node)
-            const ontologyNamespace: string | undefined = (document.parseResult.value as Ontology)?.namespace
-            if (unqualifiedName &&  ontologyNamespace) {
-                const name = this.createIRI(unqualifiedName, ontologyNamespace)
-                exports.push(this.descriptions.createDescription(node, name, document))
-            }
-        }
+    constructor(services: LangiumServices) {
+        super(services);
     }
 
-    // Creates a qualified name for the given SpecializableTerm
-    // Should only be used with terms original to the given document
-    private createIRI = (memberName: string, ontologyNamespace: string): string =>  {
-        return ontologyNamespace.substring(0, ontologyNamespace.length - 1) + memberName + '>'
-    }
-}
-
-type ParsedIRI = {
-    namespace: string,
-    memberID?: string
-}
-
-export class OMLScopeProvider extends DefaultScopeProvider {
-    override getScope(context: ReferenceInfo): Scope {
-        if (isImport(context.container))
-            return super.getScope(context)
-        const document = AstUtils.getDocument(context.container).parseResult.value as Ontology
-        const importedOntologyNamespaceToPrefix: Map<string, string|null> = new Map()
-        document.ownedImports.forEach((i) => {
-            importedOntologyNamespaceToPrefix.set(i.imported.$refText, i.prefix ? i.prefix : null)
-        })
-        
-        return this.createScope(super.getScope(context).getAllElements()
-            .filter((desc) => this.isLocalMember(desc) || this.isMemberFromImportedOntology(importedOntologyNamespaceToPrefix, desc.name))
-            .map(desc => {
-                const parsedIRI = this.parseFullIRI(desc.name)
-                return !this.isMemberFromImportedOntology(importedOntologyNamespaceToPrefix, desc.name) ? desc : [desc, {
-                ...desc,
-                name: this.getAbbreviatedIRI(parsedIRI.memberID!, importedOntologyNamespaceToPrefix.get(parsedIRI.namespace)!)
-            }]}).flat()
-        )
-    }
-
-    private isMemberFromImportedOntology = (importedOntologyNamespaces: Set<string> | Map<string, any>, IRI: string): boolean => {
-        const { namespace } = this.parseFullIRI(IRI)
-        const ret =  importedOntologyNamespaces.has(namespace)
-        // console.log(`${IRI} ${ret ? 'is': 'is not'} a member from one of our imported ontologies: ${importedOntologyNamespaces}`)
-        return ret
-    }
-        
-    private isLocalMember = (member: AstNodeDescription): boolean => {
-        const ret = !OntologicalModelingLanguageTerminals.IRI.test(member.name)
-        // console.log(`is ${member.name} a local member? ${ret}`)
-        return ret
-    }
-
-    private getAbbreviatedIRI = (memberID: string, prefix: string): string => {
-        return prefix + ':' + memberID
-    }
-
-    
-    private parseFullIRI = (fullIRI: string): ParsedIRI => {
-        // Remove trailing separator and add ending bracket
-        const sepIndex = fullIRI.lastIndexOf('#') != -1 ? fullIRI.lastIndexOf('#') : fullIRI.lastIndexOf('/')
-        const namespace = fullIRI.substring(0, sepIndex + 1) + '>'
-        if (fullIRI[sepIndex] !== '>') {
-            const memberID = fullIRI.substring(sepIndex + 1, fullIRI.length - 1)
-            return { namespace, memberID }
-        }
-        else {
-            return { namespace }
-        } 
+    override async computeExports(document: LangiumDocument, cancelToken = CancellationToken.None): Promise<AstNodeDescription[]> {
+        return [] //this.computeExportsForNode(document.parseResult.value, document, undefined, cancelToken);
     }
 }
--- oml-code/src/language/ontological-modeling-language-validator.ts
+++ oml-code/src/language/ontological-modeling-language-validator.ts
@@ -1,8 +1,8 @@
 // import type { ValidationAcceptor, ValidationChecks } from 'langium';
 // import type { OntologicalModelingLanguageAstType } from './generated/ast.js';
-import { ValidationAcceptor, ValidationChecks } from 'langium';
+import { ValidationChecks } from 'langium';
 import type { OntologicalModelingLanguageServices } from './ontological-modeling-language-module.js';
-import { OntologicalModelingLanguageAstType, ScalarEquivalenceAxiom } from './generated/ast.js';
+import { OntologicalModelingLanguageAstType } from './generated/ast.js';
 
 /**
  * Register custom validation checks.
@@ -11,7 +11,7 @@ export function registerValidationChecks(services: OntologicalModelingLanguageSe
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.OntologicalModelingLanguageValidator;
     const checks: ValidationChecks<OntologicalModelingLanguageAstType> = {
-        ScalarEquivalenceAxiom: validator.checkUniqueScalarEquivalenceProperties
+        
     };
     registry.register(checks, validator);
 }
@@ -19,34 +19,4 @@ export function registerValidationChecks(services: OntologicalModelingLanguageSe
 /**
  * Implementation of custom validations.
  */
-export class OntologicalModelingLanguageValidator {
-
-    // A ScalarEquivalenceAxiom can only have one instance of each of the
-    // properties listed in uniqueProps. This validation rule addresses
-    // the fact that optional elements in unordered groups aren't supported
-    // by langium
-    checkUniqueScalarEquivalenceProperties(axiom: ScalarEquivalenceAxiom, accept: ValidationAcceptor): void {
-        // Note: this list will need to be updated if more properties are added to the ScalarEquivalenceAxiom rule
-        const uniqueProps = [
-            'length',
-            'minLength',
-            'maxLength',
-            'pattern',
-            'language',
-            'minInclusive',
-            'minExclusive',
-            'maxInclusive',
-            'maxExclusive'
-        ]
-        
-        for (const prop of uniqueProps) {
-            // @ts-ignore
-            if (axiom[prop].length > 1) {
-                accept('error', `specifiy at most one of each property (duplicate ${prop})`, {
-                    node: axiom
-                })
-            }
-        }
-    }
-
-}
+export class OntologicalModelingLanguageValidator {}
