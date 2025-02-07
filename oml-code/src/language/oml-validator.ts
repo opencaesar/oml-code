927693653b689c3a6ef0906992ed7ed0c3f9287a
LeroyBG <leroylightning@g.ucla.edu>
2025-02-07 14:24:20 -0800

    add validation from main branch

--- oml-code/src/language/oml-validator.ts
+++ oml-code/src/language/oml-validator.ts
@@ -1,5 +1,5 @@
-import type { ValidationChecks } from 'langium';
-import type { OmlAstType } from './generated/ast.js';
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { OmlAstType, ScalarEquivalenceAxiom } from './generated/ast.js';
 import type { OmlServices } from './oml-module.js';
 
 /**
@@ -8,11 +8,41 @@ import type { OmlServices } from './oml-module.js';
 export function registerValidationChecks(services: OmlServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.OmlValidator;
-    const checks: ValidationChecks<OmlAstType> = {};
+    const checks: ValidationChecks<OmlAstType> = {
+        ScalarEquivalenceAxiom: validator.checkUniqueScalarEquivalenceProperties
+    };
     registry.register(checks, validator);
 }
 
 /**
  * Implementation of custom validations.
  */
-export class OmlValidator {}
+export class OmlValidator {
+    // A ScalarEquivalenceAxiom can only have one instance of each of the
+    // properties listed in uniqueProps. This validation rule addresses
+    // the fact that optional elements in unordered groups aren't supported
+    // by langium
+    checkUniqueScalarEquivalenceProperties(axiom: ScalarEquivalenceAxiom, accept: ValidationAcceptor): void {
+        // Note: this list will need to be updated if more properties are added to the ScalarEquivalenceAxiom rule
+        const uniqueProps = [
+            'length',
+            'minLength',
+            'maxLength',
+            'pattern',
+            'language',
+            'minInclusive',
+            'minExclusive',
+            'maxInclusive',
+            'maxExclusive'
+        ]
+        
+        for (const prop of uniqueProps) {
+            // @ts-ignore
+            if (axiom[prop].length > 1) {
+                accept('error', `specifiy at most one of each property (duplicate ${prop})`, {
+                    node: axiom
+                })
+            }
+        }
+    }
+}
