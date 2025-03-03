b295d429d115e2134a321343fa67b7b50cfce5d7
LeroyBG <leroylightning@g.ucla.edu>
2025-03-02 17:20:09 -0800

    Check correct import types from OML spec

--- oml-code/src/language/oml-validator.ts
+++ oml-code/src/language/oml-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { OmlAstType, ScalarEquivalenceAxiom } from './generated/ast.js';
+import { isDescription, isDescriptionBundle, isVocabulary, isVocabularyBundle, type OmlAstType, type Ontology, type ScalarEquivalenceAxiom } from './generated/ast.js';
 import type { OmlServices } from './oml-module.js';
 
 /**
@@ -9,7 +9,8 @@ export function registerValidationChecks(services: OmlServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.OmlValidator;
     const checks: ValidationChecks<OmlAstType> = {
-        ScalarEquivalenceAxiom: validator.checkUniqueScalarEquivalenceProperties
+        ScalarEquivalenceAxiom: validator.checkUniqueScalarEquivalenceProperties,
+        Ontology: validator.checkValidImports
     };
     registry.register(checks, validator);
 }
@@ -42,6 +43,105 @@ export class OmlValidator {
                 accept('error', `specifiy at most one of each property (duplicate ${prop})`, {
                     node: axiom
                 })
+            }
+        }
+    }
+
+    checkValidImports(ontology: Ontology, accept: ValidationAcceptor): void {
+        // OML supports several kinds of imports for each one of its ontology kinds:
+        // * Vocabulary: extends Vocabulary, uses Description
+        // * Vocabulary Bundle: extends VocabularyBundle, includes Vocabulary
+        // * Description: extends Description, uses Vocabulary
+        // * Description Bundle: extends DescriptionBundle, includes Description, uses Vocabulary, uses VocabularyBundle
+        
+        for (const ownedImport of ontology.ownedImports) {
+            // If we can't locate the node this import is referring to, an
+            // error will be given by the scope provider
+            if (!ownedImport.imported.ref) {
+                return
+            }
+            if (ownedImport.kind.extension) {
+                if (ownedImport.imported.ref.$type === ontology.$type) {
+                    return
+                } else {
+                    accept(
+                        'error',
+                        `${ontology.$type}s can extend other ${ontology.$type}s (extending ${ownedImport.imported.ref.$type})`,
+                        {node: ownedImport}
+                    )
+                }
+            }
+            
+            if (isVocabulary(ontology)) {
+                if (!ownedImport.kind.usage) {
+                    accept(
+                        'error',
+                        'Vocabularies can extend other Vocabularies and use Descriptions',
+                        {node: ownedImport}
+                    )
+                } else if (!isDescription(ownedImport.imported.ref)) {
+                    accept(
+                        'error',
+                        `Vocabularies use Descriptions (using ${ownedImport.imported.ref.$type})`,
+                        {node: ownedImport}
+                    )
+                }
+            } else if (isVocabularyBundle(ontology)) {
+                if (
+                    !ownedImport.kind.inclusion
+                ) {
+                    accept(
+                        'error',
+                        'Vocabulary Bundles can extend other Vocabulary Bundles and include Vocabularies',
+                        {node: ownedImport}
+                    )
+                } else if (!isVocabulary(ownedImport.imported.ref)) {
+                    accept(
+                        'error',
+                        `Vocabulary Bundles can include Vocabularies (including ${ownedImport.imported.ref.$type})`,
+                        {node: ownedImport}
+                    )
+                }
+            } else if (isDescription(ontology)) {
+                if (
+                    !ownedImport.kind.usage
+                ) {
+                    accept(
+                        'error',
+                        'Descriptions can extend other Descriptions and use Vocabularies',
+                        {node: ownedImport}
+                    )
+                } else if (!isVocabulary(ownedImport.imported.ref)) {
+                    accept(
+                        'error',
+                        `Descriptions can use Vocabularies (using ${ownedImport.imported.ref.$type})`,
+                        {node: ownedImport}
+                    )
+                }
+            } else if (isDescriptionBundle(ontology)) {
+                if (ownedImport.kind.inclusion) {
+                    if (!isDescription(ownedImport.imported.ref)) {
+                        accept(
+                            'error',
+                            `Description bundles can include Descriptions (including ${ownedImport.imported.ref.$type}`,
+                            {node: ownedImport}
+                        )
+                    }
+                } else if (ownedImport.kind.usage) {
+                    if (!isVocabulary(ownedImport.imported.ref) && !isVocabularyBundle(ownedImport.imported.ref)) {
+                        accept(
+                            'error',
+                            `Description bundles can use Vocabularies or Vocabulary Bundles (using ${ownedImport.imported.ref.$type})`,
+                            {node: ownedImport}
+                        )
+                    }
+                } else {
+                    accept(
+                        'error',
+                        'Description Bundles can extend Description Bundles, includes Descriptions, and use Vocabulary and Vocabulary Bundles',
+                        {node: ownedImport}
+                    )
+                }
             }
         }
     }
